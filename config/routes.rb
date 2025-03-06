5e5827e7ecb56fbc9612c922ada189360ee9f10c
bindk <sarakenny123@yahoo.com>
2025-03-07 03:04:05 +0700

    Added VideosController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post 'users/sign_in_or_sign_up', to: 'users#sign_in_or_sign_up'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :videos, only: [:index, :new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
