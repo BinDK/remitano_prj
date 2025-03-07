7025c98b32184db2476e57b9151a39d6fbf0a225
bindk <sarakenny123@yahoo.com>
2025-03-07 21:02:02 +0700

    Using callback after_create_commit to broadcast other user

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -4,4 +4,28 @@ class YoutubeVideo < ApplicationRecord
   belongs_to :user
 
   validates :title, :thumbnail, :video_type, :url, presence: true
+
+  after_create_commit :broadcast_video_notification
+
+  private
+
+  def broadcast_video_notification
+    html = render_video_notification
+    Rails.logger.info "Broadcasting notification: #{html.truncate(100)}"
+
+    ActionCable.server.broadcast('video_notifications_channel', {
+      type: 'new_video',
+      html:,
+      current_user_id: user.id
+    })
+  rescue StandardError => e
+    Rails.logger.error "Broadcast failed: #{e.message}"
+  end
+
+  def render_video_notification
+    ApplicationController.renderer.render(
+      partial: 'videos/notification',
+      locals: { video: self }
+    )
+  end
 end
--- spec/models/youtube_video_spec.rb
+++ spec/models/youtube_video_spec.rb
@@ -13,4 +13,43 @@ RSpec.describe YoutubeVideo, type: :model do
     it { should validate_presence_of(:thumbnail) }
     it { should validate_presence_of(:video_type) }
   end
+
+  describe 'callbacks' do
+    it 'calls broadcast_video_notification after create' do
+      youtube_video = build(:youtube_video)
+      expect(youtube_video).to receive(:broadcast_video_notification)
+      youtube_video.save
+    end
+  end
+
+  describe '#broadcast_video_notification' do
+    let(:youtube_video) { create(:youtube_video) }
+    let(:html) { '<div>Test notification</div>' }
+    let(:logger_double) { double('Logger') }
+
+    it 'should broadcasts to the video_notifications_channel' do
+      allow(youtube_video).to receive(:render_video_notification).and_return(html)
+
+      expect(ActionCable.server).to receive(:broadcast).with(
+        'video_notifications_channel',
+        {
+          type: 'new_video',
+          html:,
+          current_user_id: youtube_video.user.id
+        }
+      )
+
+      youtube_video.send(:broadcast_video_notification)
+    end
+
+    it 'should handles exceptions' do
+      allow(youtube_video).to receive(:render_video_notification).and_raise(StandardError.new('Test error'))
+      allow(Rails).to receive(:logger).and_return(logger_double)
+
+      expect(logger_double).to receive(:error).with('Broadcast failed: Test error')
+      expect {
+        youtube_video.send(:broadcast_video_notification)
+      }.not_to raise_error
+    end
+  end
 end
