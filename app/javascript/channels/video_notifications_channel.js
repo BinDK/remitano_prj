868a8ad6ec994cc3f97cc12560e502d6a6dee869
bindk <sarakenny123@yahoo.com>
2025-03-07 20:28:53 +0700

    Added video_notification_channel js component

---
+++ app/javascript/channels/video_notifications_channel.js
@@ -0,0 +1,36 @@
+import consumer from "channels/consumer"
+
+consumer.subscriptions.create("VideoNotificationsChannel", {
+  connected() {
+    console.log("Connected to video notifications");
+  },
+
+  disconnected() {
+    console.log("Disconnected from video notifications");
+  },
+
+  received(data) {
+    console.log("Received data:", data);
+    if (data.type === 'new_video') {
+      const currentUserMetaTag = document.querySelector('meta[name="current-user-id"]');
+      const currentUserId = currentUserMetaTag ? currentUserMetaTag.getAttribute('content') : null;
+
+      console.log("Current user:", currentUserId, "Creator:", data.current_user_id);
+
+      if (!currentUserId || currentUserId !== data.current_user_id.toString()) {
+        this.appendNotification(data.html);
+      }
+    }
+  },
+
+  appendNotification(html) {
+    console.log("Appending notification");
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = html;
+    document.body.appendChild(tempDiv);
+
+    setTimeout(() => {
+      tempDiv.remove();
+    }, 5000);
+  }
+});
