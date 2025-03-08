da982deb7887e2a40b565cd191598c0f47903c9c
bindk <sarakenny123@yahoo.com>
2025-03-08 20:19:04 +0700

    Update: Enqueue Url to Background Job

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -5,43 +5,42 @@ RSpec.describe 'Videos', type: :request do
   let(:valid_attributes) { { youtube_video: { url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' } } }
   let(:invalid_attributes) { { youtube_video: { url: 'invalid-url' } } }
 
-  let(:title) { 'Example Video Title' }
-  let(:thumbnail) { 'https://example.com/thumbnail.jpg' }
-  let(:mocked_html) do
-    <<-HTML
-    <html>
-      <head>
-        <meta property="og:title" content="#{title}" />
-        <meta property="og:image" content="#{thumbnail}" />
-      </head>
-    </html>
-    HTML
-  end
-
   describe 'POST /videos' do
     context 'when user is logged in' do
       before { sign_in user }
 
       context 'with valid params' do
-        it 'creates a new YoutubeVideo and redirects to the videos index' do
+        it 'should enqueues a ProcessVideoJob and redirects to the videos index reight away' do
           expect {
             post videos_path, params: valid_attributes
-          }.to change(YoutubeVideo, :count).by(1)
+          }.to have_enqueued_job(ProcessVideoJob).with(valid_attributes[:youtube_video][:url], user)
 
           expect(response).to redirect_to(videos_path)
           follow_redirect!
-          expect(response.body).to include('Video was successfully shared!')
+          expect(response.body).to include('Video submitted and being processed!')
         end
       end
 
-      context 'with invalid params' do
-        it 'does not create a new YoutubeVideo and re-renders the new template' do
+      context 'with blank URL' do
+        it 'does not enqueue a job and re-renders the new template' do
+          blank_url_attributes = { youtube_video: { url: '' } }
+
           expect {
-            post videos_path, params: invalid_attributes
-          }.not_to change(YoutubeVideo, :count)
+            post videos_path, params: blank_url_attributes
+          }.not_to have_enqueued_job(ProcessVideoJob)
 
           expect(response).to have_http_status(:unprocessable_entity)
-          expect(response.body).to include('Share Video')
+          expect(response.body).to include('URL cannot be blank')
+        end
+      end
+
+      context 'with invalid URL' do
+        it 'enqueues a job to be validated in the background' do
+          expect {
+            post videos_path, params: invalid_attributes
+          }.to have_enqueued_job(ProcessVideoJob)
+
+          expect(response).to redirect_to(videos_path)
         end
       end
     end
