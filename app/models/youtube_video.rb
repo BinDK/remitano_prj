9d625f3066141c9680c58712ee7de7f7126f2e80
bindk <sarakenny123@yahoo.com>
2025-03-08 21:27:13 +0700

    Added new shared render video card as a data for boradcasting

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,6 +1,12 @@
 class YoutubeVideo < ApplicationRecord
   self.table_name = 'videos'
 
+  YOUTUBE_PATTERNS = [
+    /youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/,
+    /youtu\.be\/([a-zA-Z0-9_-]+)/,
+    /youtube\.com\/shorts\/([a-zA-Z0-9_-]+)/
+  ]
+
   belongs_to :user
 
   validates :title, :thumbnail, :video_type, :url, presence: true
@@ -11,12 +17,15 @@ class YoutubeVideo < ApplicationRecord
 
   def broadcast_video_notification
     html = render_video_notification
+    card = render_video_card
+
     Rails.logger.info "Broadcasting notification: #{html.truncate(100)}"
 
     ActionCable.server.broadcast('video_notifications_channel', {
       type: 'new_video',
       html:,
-      current_user_id: user.id
+      current_user_id: user.id,
+      card:
     })
   rescue StandardError => e
     Rails.logger.error "Broadcast failed: #{e.message}"
@@ -25,6 +34,13 @@ class YoutubeVideo < ApplicationRecord
   def render_video_notification
     ApplicationController.renderer.render(
       partial: 'videos/notification',
+      locals: { message: "#{user.email} shared a new video: #{title.truncate(40)}", type: 'success' }
+    )
+  end
+
+  def render_video_card
+    ApplicationController.renderer.render(
+      partial: 'videos/video',
       locals: { video: self }
     )
   end
--- spec/models/youtube_video_spec.rb
+++ spec/models/youtube_video_spec.rb
@@ -25,17 +25,20 @@ RSpec.describe YoutubeVideo, type: :model do
   describe '#broadcast_video_notification' do
     let(:youtube_video) { create(:youtube_video) }
     let(:html) { '<div>Test notification</div>' }
+    let(:card) { '<div>Video card</div>' }
     let(:logger_double) { double('Logger') }
 
-    it 'should broadcasts to the video_notifications_channel' do
+    it 'should broadcasts to the video_notifications_channel with html and card' do
       allow(youtube_video).to receive(:render_video_notification).and_return(html)
+      allow(youtube_video).to receive(:render_video_card).and_return(card)
 
       expect(ActionCable.server).to receive(:broadcast).with(
         'video_notifications_channel',
         {
           type: 'new_video',
           html:,
-          current_user_id: youtube_video.user.id
+          current_user_id: youtube_video.user.id,
+          card:
         }
       )
 
